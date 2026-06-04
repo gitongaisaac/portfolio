d90b5baa3d342fbee7921c5b28d0d16e120355a1
Isaac Gitonga <isaacggitonga@gmail.com>
2026-06-04 22:28:19 +0300

    update public repo information; live url, and links

--- src/assets/data/portfolio.ts
+++ src/assets/data/portfolio.ts
@@ -62,5 +62,5 @@ export const portfolio: Project = {
   ],
 
   repoUrl: "https://github.com/gitongaisaac/portfolio",
-  liveUrl: "https://gitongaisaac.netlify.app"
+  liveUrl: "https://isaacgitonga.com"
 };
